56e65276fb7bfa257185e4dbca24fc95ffbceed2
Moisés Oliveira <moisesoliver016@gmail.com>
2016-05-04 04:17:32 -0500

    Update Build

--- build.py
+++ build.py
@@ -48,31 +48,47 @@ manifestVersion="1.0">
 RT_MANIFEST = 24
 
 setup(
-    options = {'py2exe': {'bundle_files': 1, 
-						  'compressed': True,
-						  'optimize': 1,
-						  'dll_excludes': ['w9xpopen.exe']
-						  }
-			  },
-    windows = [{'script'		 : "main.py",#Nome do projeto.py para ser compilado
-			   'name'            : "pyBotnet", #Defina com suas variáveis
-			   'description'     : "Botnet Desenvolvida em Python", #Defina com suas variáveis
-			   'version'         : "1.0", #Defina com suas variáveis
-			   'company_name'    : "Unknowns", #Defina com suas variáveis
-			   'copyright'       : "Copyright 2016 Unknowns Systems Incorporated", #Defina com suas variáveis
-			   'icon_resources'  : [(1, "icons\\adobe.ico")], #Icone - Comente essa linha caso nao queira um icone, ou troque para o icone da pasta icons para qual icone desejar
-			   #'other_resources' : [(RT_MANIFEST, 1, manifest_template)], #Descomente essa linha para o programa rodar com Privilegio de adminstrador
-			   }],
-    zipfile = None,
+	options = {
+		'py2exe': {
+			'bundle_files': 1, 
+			'compressed': True,
+			'optimize': 1,
+			'dll_excludes': ['w9xpopen.exe']
+		}
+	},
+	windows = [{	
+			'script'	: "main.py",#Nome do arquivo.py para ser compilado
+			'name'		: "pyBotnet", #Defina com suas variáveis
+			'description'	: "Botnet Desenvolvida em Python", #Defina com suas variáveis
+			'version'	: "1.0", #Defina com suas variáveis
+			'company_name'	: "Unknowns", #Defina com suas variáveis
+			'copyright'	: "Copyright 2016 Unknowns Systems Incorporated", #Defina com suas variáveis
+			'icon_resources': [(1, "icons\\python.ico")], #Icone - Comente essa linha caso nao queira um icone, ou troque para o icone da pasta icons para qual icone desejar
+			#'other_resources' : [(RT_MANIFEST, 1, manifest_template)], #Descomente essa linha para o programa rodar com Privilegio de adminstrador
+	}],
+	zipfile = None,
 )
 """
 Codado by Moisés Oliver
-Facebook: https://www.facebook.com/profile.php?id=100010829880519
 """
-print "\n", "Aquivo compilado com sucesso: ", "\n"
-print "Rodando UPX -> Empacotando o execultavel", "\n"
+print "\nAquivo compilado com sucesso:\n\n\
+Rodando UPX em subprocess\n\
+-> Empacotando o execultavel\n\
+-> Modo de compressao ultra-brute\n\
+-> Isso pode demorar um pouco\n"
 #Aqui o exe sera comprimido com o upx
+tam_antes = os.path.getsize("dist\main.exe")
+
 local = os.getcwd()
-run =  local + "\upx.exe","dist\main.exe"
-subprocess.call(run)
+command = local + "\upx.exe --ultra-brute", "dist\main.exe"
+processo = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+processo.wait()
+
+tam_depois = os.path.getsize("dist\main.exe")
+tam_z = tam_antes - tam_depois
+
+print "Tamanho Antes: %s kb" % tam_antes
+print "Tamanho Novo : %s kb" % os.path.getsize("dist\main.exe")
+
+
 
